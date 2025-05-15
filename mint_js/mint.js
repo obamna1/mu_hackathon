e7f4b4dc1a4431d405b8cee2b69e4d43880f6bcc
kha1n3vol3 <khunt@starficient.com>
2025-05-14 23:14:29 -0400

    fix(mint_js): import default export of bs58 and use bs58.decode for SOLANA_SECRET_KEY decoding

--- mint_js/mint.js
+++ mint_js/mint.js
@@ -1,6 +1,5 @@
 ﻿require('dotenv').config();
-const bs58 = require('bs58');
-const decodeBase58 = typeof bs58 === 'function' ? bs58 : bs58.decode;
+const bs58 = require('bs58').default;
 const fs = require('fs');
 const {
     Connection, Keypair, PublicKey, Transaction, SystemProgram,
@@ -17,7 +16,7 @@ const TOKEN_METADATA_PROGRAM_ID = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzj
 const metadata = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
 
 const connection = new Connection(process.env.SOLANA_URL, 'confirmed');
-const secretKeyDecoded = decodeBase58(process.env.SOLANA_SECRET_KEY);
+const secretKeyDecoded = bs58.decode(process.env.SOLANA_SECRET_KEY);
 const payer = Keypair.fromSecretKey(secretKeyDecoded);
 
 (async () => {
